9e7f85164db6ecb2e768dbf1b108ba125b750d7f
Rafael Martins Paiva <rafael.martins.paiva0@gmail.com>
2025-11-23 18:40:53 -0300

    .

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -39,7 +39,6 @@ Já participei de alguns [hackathons](/#hackathons) e gosto de transformar ideia
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
-    { href: "/blog", icon: NotebookIcon, label: "Blog" },
   ],
   contact: {
     email: "hello@example.com",
@@ -88,7 +87,7 @@ Já participei de alguns [hackathons](/#hackathons) e gosto de transformar ideia
       dates: "Jan 2024 - Feb 2024",
       active: true,
       description:
-        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+        "With the release of the [OpenAI GPT Store](https://openai.com/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
       technologies: [
         "Next.js",
         "Typescript",
