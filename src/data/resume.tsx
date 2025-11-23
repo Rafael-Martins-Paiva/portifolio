edd7ca161215dd8aed25d60088cb1c66a65e2aa5
Rafael Martins Paiva <rafael.martins.paiva0@gmail.com>
2025-11-23 18:28:20 -0300

    .

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -81,31 +81,6 @@ Já participei de alguns [hackathons](/#hackathons) e gosto de transformar ideia
       },
     },
   },
-
-  work: [
-    {
-      company: "Atomic Finance",
-      href: "https://atomic.finance",
-      badges: [],
-      location: "Remote",
-      title: "Bitcoin Protocol Engineer",
-      logoUrl: "/atomic.png",
-      start: "May 2021",
-      end: "Oct 2022",
-      description:
-        "Implemented the Bitcoin discreet log contract (DLC) protocol specifications as an open source Typescript SDK. Dockerized all microservices and setup production kubernetes cluster. Architected a data lake using AWS S3 and Athena for historical backtesting of bitcoin trading strategies. Built a mobile app using react native and typescript.",
-    },
-  ],
-  education: [
-    {
-      school: "Buildspace",
-      href: "https://buildspace.so",
-      degree: "s3, s4, sf1, s5",
-      logoUrl: "/buildspace.jpg",
-      start: "2023",
-      end: "2024",
-    },
-  ],
   projects: [
     {
       title: "Chat Collect",
